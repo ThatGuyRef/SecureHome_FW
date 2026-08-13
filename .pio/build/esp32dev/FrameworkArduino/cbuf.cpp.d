9e9c6785c8ed46ce9a25ff11ec3a8302faad9f6d
ThatGuyRef <navarrorevi@gmail.com>
2026-08-13 22:50:24 +0700

    Macboook

---
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/reffi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/reffi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ include/rahasia.h
@@ -0,0 +1,8 @@
+#pragma once
+#define WIFI_SSID   "Rumah Ibu"
+#define WIFI_PASS   "RumahIbu"
+#define SB_URL      "https://qnhwnmnfyplecqjfzcet.supabase.co"
+#define SB_ANON     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuaHdubW5meXBsZWNxamZ6Y2V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2MzE4ODUsImV4cCI6MjEwMDIwNzg4NX0.lBaGqsDiMr1Ya5lU4fC3rfIjURPF6gsd1D4RQI6ZrTQ"
+#define DEV_EMAIL   "device01@securehome.id"
+#define DEV_PASS    "6f.TvhH3wW@6Pwv"
+#define DEVICE_KEY  "securehome-a1b2-alarm"
